25c825075961c95cb45c0ae2bb8048848084b37f
Do-Duc-Anh <155513734+Do-Duc-Anh@users.noreply.github.com>
2024-04-13 20:49:31 +0700

    Update trangchu.js

--- js/trangchu.js
+++ js/trangchu.js
@@ -29,9 +29,7 @@ window.onload = function () {
 	for (var t of tags) addTags(t, "index.html?search=" + t);
 
 	// Thêm danh sách hãng điện thoại
-	var company = ["Apple.jpg", "Samsung.jpg", "Oppo.jpg", "Nokia.jpg", "Huawei.jpg", "Xiaomi.png",
-		"Realme.png", "Vivo.jpg", "Philips.jpg", "Mobell.jpg", "Mobiistar.jpg", "Itel.jpg",
-		"Coolpad.png", "HTC.jpg", "Motorola.jpg"
+	var company = [
 	];
 	for (var c of company) addCompany("img/company/" + c, c.slice(0, c.length - 4));
 
